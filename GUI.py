45812ef5d0cf0b8f4787f478328832e851a1031a
heEXDe <m.a.p.zaleski@protonmail.com>
2020-11-09 00:23:28 +0100

    Update GUI.py, logic.py, and tic-tac-toe.py

--- GUI.py
+++ GUI.py
@@ -4,14 +4,19 @@ from tkinter import *
 
 class App(Tk):
 
-    def gui_build(self):
-        # global root
-        global lbl_you_play_as
-        # root = Tk()
+    def __init__(self, you_play_as, win_or_loose):
+        super().__init__()
         self.title('Tic-tac-toe')
         self.resizable(0, 0)
-        lbl_you_play_as = Label(self, text='You play as: ')
+        self.you_play_as = you_play_as
+        self.win_or_loose = win_or_loose
+
+    def gui_build(self):
+        # global lbl_you_play_as
+        lbl_you_play_as = Label(self, text='You play as: ' + self.you_play_as)
         lbl_you_play_as.grid(row=0, column=0, columnspan=3, pady=5)
+        lbl_win_loose = Label(self, text=self.win_or_loose)
+        lbl_win_loose.grid(row=4, column=0, columnspan=3, pady=5)
         btn_1 = Button(self, text='', height=5, width=7)
         btn_1.grid(row=1, column=0)
         btn_2 = Button(self, text='', height=5, width=7)
@@ -30,14 +35,17 @@ class App(Tk):
         btn_8.grid(row=3, column=1)
         btn_9 = Button(self, text='', height=5, width=7)
         btn_9.grid(row=3, column=2)
-        lbl_win_loose = Label(self, text='')
-        lbl_win_loose.grid(row=4, column=0, columnspan=3, pady=5)
         btn_new_game = Button(self, text='New game', height=3, width=21)
         btn_new_game.grid(row=5, column=0, columnspan=3, padx=5)
+
+    """
+        def create_button(self, name, text, g_row, g_col, pad_x, pad_y, heig, widt):
+        self.name = 
+    """
 
     def gui_start(self):
         self.gui_build()
         self.mainloop()
 
 
-that_app = App()
+that_app = App('', '')
--- logic.py
+++ logic.py
@@ -7,7 +7,7 @@ class Logic:
     def cross_or_circle(self):
         global c_or_c
         c_or_c = random.choice(['X', 'O'])
-        GUI.that_app.root.lbl_you_play_as.change(text='You play as: ' + str(c_or_c))
+        GUI.that_app.you_play_as = str(c_or_c)
 
 
 logic1 = Logic()
