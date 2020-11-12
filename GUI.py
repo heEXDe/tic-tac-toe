30daf299b109aef89f428be3ea014f44d5a87624
heEXDe <m.a.p.zaleski@protonmail.com>
2020-11-12 19:56:36 +0100

    Update GUI.py and logic.py

--- GUI.py
+++ GUI.py
@@ -21,6 +21,7 @@ class App(Tk):
         self.resizable(0, 0)
         self.you_play_as = you_play_as
         self.win_or_loose = win_or_loose
+        self.lst_points = [1, 1, 1, 1, 1, 1, 1, 1, 1]
         self.btn1 = Button(text='', height=5, width=7, command=lambda: logic.logic1.display_sign_on_btn(self.btn1))
         self.btn2 = Button(text='', height=5, width=7, command=lambda: logic.logic1.display_sign_on_btn(self.btn2))
         self.btn3 = Button(text='', height=5, width=7, command=lambda: logic.logic1.display_sign_on_btn(self.btn3))
@@ -49,7 +50,7 @@ class App(Tk):
         self.btn8.grid(row=3, column=1)
         self.btn9.grid(row=3, column=2)
 
-        btn_new_game = Button(self, text='New game', height=3, width=21)
+        btn_new_game = Button(self, text='New game', height=3, width=21, command=lambda: logic.logic1.new_game())
         btn_new_game.grid(row=5, column=0, columnspan=3, padx=5)
 
     # a function starting the GUI
--- logic.py
+++ logic.py
@@ -12,6 +12,48 @@ class Logic:
 
     def display_sign_on_btn(self, btn):
         btn.config(text=self.c_or_c)
+        self.ai_move()
+
+    def ai_move(self):
+        btext = GUI.that_app.btn1['text']
+        self.add_p_to_lst_points(btext, 0)
+        btext = GUI.that_app.btn2['text']
+        self.add_p_to_lst_points(btext, 1)
+        btext = GUI.that_app.btn3['text']
+        self.add_p_to_lst_points(btext, 2)
+        btext = GUI.that_app.btn4['text']
+        self.add_p_to_lst_points(btext, 3)
+        btext = GUI.that_app.btn5['text']
+        self.add_p_to_lst_points(btext, 4)
+        btext = GUI.that_app.btn6['text']
+        self.add_p_to_lst_points(btext, 5)
+        btext = GUI.that_app.btn7['text']
+        self.add_p_to_lst_points(btext, 6)
+        btext = GUI.that_app.btn8['text']
+        self.add_p_to_lst_points(btext, 7)
+        btext = GUI.that_app.btn9['text']
+        self.add_p_to_lst_points(btext, 8)
+        print(GUI.that_app.lst_points)
+
+    def add_p_to_lst_points(self, bt, position):
+        if bt == 'O':
+            GUI.that_app.lst_points[position] = 2
+        elif bt == 'X':
+            GUI.that_app.lst_points[position] = 3
+        else:
+            GUI.that_app.lst_points[position] = 1
+
+    def new_game(self):
+        GUI.that_app.btn1.config(text="")
+        GUI.that_app.btn2.config(text="")
+        GUI.that_app.btn3.config(text="")
+        GUI.that_app.btn4.config(text="")
+        GUI.that_app.btn5.config(text="")
+        GUI.that_app.btn6.config(text="")
+        GUI.that_app.btn7.config(text="")
+        GUI.that_app.btn8.config(text="")
+        GUI.that_app.btn9.config(text="")
+        GUI.that_app.lst_points = [1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 
 coc = random.choice(['X', 'O'])
